ec8d1e53c15980b09ffb7880a2da3b605109d9b2
Christina <184446177+ckim53@users.noreply.github.com>
2025-12-09 20:59:28 -0800

    Add CSS attribute to Mantine for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<MantineProvider withGlobalStyles withNormalizeCSS>
+		<MantineProvider withGlobalStyles withNormalizeCSS forceColorScheme="dark">
 			<App />
 		</MantineProvider>
 	</StrictMode>
